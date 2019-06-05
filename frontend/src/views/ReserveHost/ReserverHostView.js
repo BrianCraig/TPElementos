eb71316c5074cd59bbb1c30fb766b8d4a4bd17c1
Brian Craig <briancraigok@gmail.com>
2019-06-05 18:52:21 -0300

    revert i broke something

--- frontend/src/views/ReserveHost/ReserverHostView.js
+++ frontend/src/views/ReserveHost/ReserverHostView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SpecificReserve } from './SpecificReserve';
 
-export const ReserveHostView = (reserveList, a}) =>
+export const ReserveHostView = ({reserveList}) =>
     <div>
         <h1>Reserve List for Today</h1>
         <div>
