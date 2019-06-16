7e3d44a96fab2c6f3489bbbd821bbd78a2ce87eb
Franco <neira.franco.ariel@gmail.com>
2019-06-16 00:30:54 -0300

    Added Store and modified Router and Index configurations.
    Co-authored-by: Brian <briancraigok@gmail.com>

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 import { Router as ReachRouter } from "@reach/router";
 import { ReserveHostComponentConnector } from "./views/ReserveHost/ReserverHostView";
 import { HomeView } from "./views/Home/HomeView";
 import { ProfileHostView } from "./views/ProfileHost/ProfileHostView";
+import { Consumer, loadData } from "./state/store"
+
+export const RouterComponent = ({dataLoaded, hosts}) => {
+  useEffect(() => {
+    if(dataLoaded) return;
+    loadData().then(() => {})
+  })
+  if(!dataLoaded){
+    return <p>Cargando</p>
+  }
+  return (
+    <ReachRouter>
+      <HomeView path='/' />
+      <ProfileHostView path='/host/profile' />
+      <ReserveHostComponentConnector path='/host/calendar'/>
+    </ReachRouter>
+  )
+}
 
 export const Router = () =>
-  <ReachRouter>
-    <HomeView path='/' />
-    <ProfileHostView path='/host/profile' />
-    <ReserveHostComponentConnector path='/host/calendar'/>
-  </ReachRouter>
+  <Consumer>
+    {({dataLoaded, hosts}) => (
+      <RouterComponent
+        dataLoaded={dataLoaded}
+        hosts={hosts} />
+    )}
+  </Consumer>
