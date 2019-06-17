a3941230c7069a66b9e348d8c36f38d58fa4801a
Brian Craig <briancraigok@gmail.com>
2019-06-17 13:28:03 -0300

    moved render from ReserveHostView Connector to Component

--- frontend/src/views/ReserveHost/ReserverHostView.js
+++ frontend/src/views/ReserveHost/ReserverHostView.js
@@ -2,19 +2,37 @@ import React from "react";
 import { SpecificReserve } from "./SpecificReserve";
 import Typography from "@material-ui/core/Typography";
 import { getHosts } from "../../api/connector";
-import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker
+} from "@material-ui/pickers";
+import moment from "moment";
 
 export const sameDay = (date1, date2) =>
   date1.getFullYear() === date2.getFullYear() &&
   date1.getMonth() === date2.getMonth() &&
   date1.getDate() === date2.getDate();
 
-export const reserveDate = (reserve) => new Date(reserve.dayHour)
+export const reserveDate = reserve => new Date(reserve.dayHour);
 
-export const ReserveHostView = ({ reserveList }) => (
+export const ReserveHostView = ({ reserveList, day, changeDay }) => (
   <div>
-    <Typography variant="h4">Reserve List for Today</Typography>
+    <Typography variant="h4">
+      Reserve List for {moment(day).format("LL")}
+    </Typography>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <KeyboardDatePicker
+        margin="normal"
+        id="mui-pickers-date"
+        label="Reservation date"
+        value={day}
+        onChange={changeDay}
+        KeyboardButtonProps={{
+          "aria-label": "change date"
+        }}
+      />
+    </MuiPickersUtilsProvider>
     <div>
       {reserveList.map(reserve => (
         <SpecificReserve key={reserve.dayHour} reserve={reserve} />
@@ -36,31 +54,19 @@ export class ReserveHostComponentConnector extends React.PureComponent {
     this.setState({ hosts: await getHosts() });
   }
 
-  changeDay = (date) => this.setState({day: date})
+  changeDay = date => this.setState({ day: date });
 
   render() {
     if (!this.state.hosts) return <p>Cargando</p>;
-    const reservesForDate = this.state.hosts[0].field.calendar.filter(reserve => sameDay(reserveDate(reserve), this.state.day))
+    const reservesForDate = this.state.hosts[0].field.calendar.filter(reserve =>
+      sameDay(reserveDate(reserve), this.state.day)
+    );
     return (
-      <>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <KeyboardDatePicker
-          margin="normal"
-          id="mui-pickers-date"
-          label="Reservation date"
-          value={this.state.day}
-          onChange={this.changeDay}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-      </MuiPickersUtilsProvider>
-      <ReserveHostView 
-        changeDay={this.changeDay} 
-        reserveList={reservesForDate} 
-        day={this.state.day} 
+      <ReserveHostView
+        changeDay={this.changeDay}
+        reserveList={reservesForDate}
+        day={this.state.day}
       />
-      </>
     );
   }
 }
